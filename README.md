93413f406d0ac9d409e4fcf0e2630a6213fdb16c
John Gorter <john.gorter@gmail.com>
2021-03-15 16:17:09 +0100

    init

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# helloworld
+# huiswerk
+https://flutter.dev/docs/development/ui/interactive#:~:text=A%20widget%20is%20either%20stateful,are%20examples%20of%20stateless%20widgets.
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- lezen over stateless en statefull
+- beginscherm van de voetbal applicatie maken of de´repo clonen en de code snappen
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,58 @@
 import 'package:flutter/material.dart';
-import 'App.dart';
+
+import 'dart:async';
+
+class Teller extends StatefulWidget {
+
+  @override
+  _TellerState createState() => _TellerState();
+}
+
+class _TellerState extends State<Teller> {
+  int counter = 0;
+
+  @override void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      counter = counter + 1;
+      print("counter hoog ik op...");
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(child: Center(child: Text("teller: ${counter}")));
+  }
+
+  dispose() {
+    print("i am dying...");
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Container(child: Teller())));
+  }
+}
 
 main() {
   runApp(MyApp());
